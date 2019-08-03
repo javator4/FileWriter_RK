6b5fa78370e43303f8c928fbb991c0d00fc255ee
Ryszard Kozakiewicz <ryszard.ko1111@gmail.com>
2019-08-03 13:34:54 +0200

    added four threats

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -1,13 +1,91 @@
 package pl.sda.filewriter;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    static class Task1 implements Runnable {
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("Wątek 1: " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    static class Task2 implements Runnable {
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("Wątek 2: " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+    }
+
+    static class Task3 implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("Wątek 3: " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class Task4 implements Runnable {
+
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("Wątek 4: " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        Task3 task3 = new Task3();
+        Task4 task4 = new Task4();
+
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
+        Thread thread4 = new Thread(task4);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
     }
 }
